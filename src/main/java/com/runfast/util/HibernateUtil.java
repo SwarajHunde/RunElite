316e15d14d11481a6767bb20523fabee9c8e9540
Swaraj Hunde <swarajhunde@gmail.com>
2026-01-21 14:04:57 +0530

    made changes in hibernateutil.java

--- src/main/java/com/runfast/util/HibernateUtil.java
+++ src/main/java/com/runfast/util/HibernateUtil.java
@@ -20,24 +20,60 @@ public class HibernateUtil {
 
         if (sessionFactory == null) {
             try {
-                String dbUrl = System.getenv("DB_URL");
-                String dbUser = System.getenv("DB_USERNAME");
-                String dbPass = System.getenv("DB_PASSWORD");
-
-                if (dbUrl == null || dbUser == null || dbPass == null) {
+                // Debug: Print available environment variables
+                System.out.println("=== Checking Environment Variables ===");
+                System.out.println("MYSQLHOST: " + System.getenv("MYSQLHOST"));
+                System.out.println("MYSQLPORT: " + System.getenv("MYSQLPORT"));
+                System.out.println("MYSQLDATABASE: " + System.getenv("MYSQLDATABASE"));
+                System.out.println("MYSQLUSER: " + System.getenv("MYSQLUSER"));
+                System.out.println("MYSQLPASSWORD available: " + (System.getenv("MYSQLPASSWORD") != null));
+                
+                // Get Railway MySQL variables
+                String mysqlHost = System.getenv("MYSQLHOST");
+                String mysqlPort = System.getenv("MYSQLPORT");
+                String mysqlDatabase = System.getenv("MYSQLDATABASE");
+                String mysqlUser = System.getenv("MYSQLUSER");
+                String mysqlPassword = System.getenv("MYSQLPASSWORD");
+                
+                // Validate required variables
+                if (mysqlHost == null || mysqlDatabase == null || mysqlUser == null || mysqlPassword == null) {
                     throw new RuntimeException(
-                        "Missing DB environment variables: DB_URL / DB_USERNAME / DB_PASSWORD"
+                        "Missing MySQL environment variables from Railway. " +
+                        "Required: MYSQLHOST, MYSQLDATABASE, MYSQLUSER, MYSQLPASSWORD"
                     );
                 }
+                
+                // Build JDBC URL for Railway MySQL
+                String jdbcUrl = "jdbc:mysql://" + mysqlHost + ":" + 
+                               (mysqlPort != null ? mysqlPort : "3306") + 
+                               "/" + mysqlDatabase + 
+                               "?useSSL=false&allowPublicKeyRetrieval=true" +
+                               "&serverTimezone=UTC&useUnicode=true&characterEncoding=UTF-8";
+                
+                System.out.println("Using JDBC URL: " + jdbcUrl.replace(mysqlPassword, "******"));
+                System.out.println("Username: " + mysqlUser);
 
                 Properties props = new Properties();
                 props.put("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
-                props.put("hibernate.connection.url", dbUrl);
-                props.put("hibernate.connection.username", dbUser);
-                props.put("hibernate.connection.password", dbPass);
+                props.put("hibernate.connection.url", jdbcUrl);
+                props.put("hibernate.connection.username", mysqlUser);
+                props.put("hibernate.connection.password", mysqlPassword);
                 props.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
                 props.put("hibernate.hbm2ddl.auto", "update");
                 props.put("hibernate.show_sql", "true");
+                
+                // Important: Add connection properties for Railway MySQL
+                props.put("hibernate.connection.provider_class", "org.hibernate.c3p0.internal.C3P0ConnectionProvider");
+                props.put("hibernate.c3p0.min_size", "5");
+                props.put("hibernate.c3p0.max_size", "20");
+                props.put("hibernate.c3p0.timeout", "1800");
+                props.put("hibernate.c3p0.max_statements", "50");
+                props.put("hibernate.c3p0.idle_test_period", "300");
+                props.put("hibernate.c3p0.acquire_increment", "2");
+                
+                // Additional MySQL optimizations
+                props.put("hibernate.connection.characterEncoding", "UTF-8");
+                props.put("hibernate.connection.useUnicode", "true");
 
                 Configuration configuration = new Configuration();
                 configuration.setProperties(props);
@@ -47,8 +83,10 @@ public class HibernateUtil {
                 configuration.addAnnotatedClass(GroupMember.class);
 
                 sessionFactory = configuration.buildSessionFactory();
+                System.out.println("Hibernate SessionFactory created successfully!");
 
             } catch (Exception e) {
+                System.err.println("Failed to initialize Hibernate:");
                 e.printStackTrace();
                 throw new RuntimeException("Failed to initialize Hibernate", e);
             }
